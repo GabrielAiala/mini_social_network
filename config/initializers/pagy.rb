5c4efc978c8aedb1c769da98faec5b9fe2208eb5
Gabriel Gomes Aiala <ggaiala@hotmail.com>
2026-09-25 16:20:44 -0300

    fix pagination

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,3 +1,7 @@
 require "pagy/extras/metadata"
+require "pagy/extras/overflow"
+
 Pagy::DEFAULT[:items_param] = :per_page
-Pagy::DEFAULT[:items] = 10  # tamanho padrão da página
+Pagy::DEFAULT[:items] = 10
+Pagy::DEFAULT[:overflow] = :empty_page
+Pagy::DEFAULT[:metadata] = [ :scaffold_url, :page, :pages, :in, :from, :to, :count, :prev_url, :next_url, :first_url, :last_url ]
